1a75ab83dd0ebe8395bb3a2178ab70a462ddc722
venkat5698 <vmuthyala@belcan.com>
2023-08-04 11:24:32 +0530

    First Commit Id

--- AssetTracker/AssetTracker/settings.py
+++ AssetTracker/AssetTracker/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'AssetTracker.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'asset',
+        'NAME': 'asset_tracker',
         'USER': 'root',
         'PASSWORD': 'Venkat@belcan90',
         'HOST': 'localhost',
@@ -121,11 +121,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    "/var/www/static/",
-]
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+STATIC_ROOT = os.path.join(BASE_DIR, 'login')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
